2c9ba5adec701130e0e5ba1f1b409703daa132e5
raihan <raihan@aventusinformatics.com>
2023-07-03 22:31:35 +0530

    sdf

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to my React TypeScript project!</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './App.tsx',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(ts|tsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react'],
+                    },
+                },
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        port: 3000,
+    },
+};
